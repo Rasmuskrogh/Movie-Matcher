bef8d9e7ec0933d710410a297bc12de593f3f68b
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2023-12-16 23:09:49 +0100

    trying to fix postgres connection

--- movie-matcher/app/register/page.tsx
+++ movie-matcher/app/register/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Register = () => {
+  return <div>Register</div>;
 };
 
-export default page;
+export default Register;
