f0b27cb7d0a6015f91b61efe2a87f858eaf1a8bb
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2023-12-19 17:18:28 +0100

    started on register page, design and function

--- movie-matcher/app/register/page.tsx
+++ movie-matcher/app/register/page.tsx
@@ -1,7 +1,72 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  async function createAccount(formData: FormData) {
+    "use server";
+    const rawFormData = {
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(rawFormData);
+  }
+
+  return (
+    <div className="w-100% h-[550px] bg-slate-800 text-white p-2">
+      <h1>Register form</h1>
+      <form action={createAccount}>
+        <div>
+          <label htmlFor="">First name: </label>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            className="text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="">Last name: </label>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            className="text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="">Username: </label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            className="text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="">Email: </label>
+          <input id="email" name="email" type="email" className="text-black" />
+        </div>
+        <div>
+          <label htmlFor="">Password: </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            className="text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="">Repeat password: </label>
+          <input type="password" className="text-black" />
+        </div>
+        <div>
+          <input type="submit" value="Register" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
